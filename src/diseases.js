382309057cd551825a04a2e2a2b5d6d3a231a153
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2023-07-31 23:29:25 +0300

    added video package for diseases

--- src/diseases.js
+++ src/diseases.js
@@ -247,7 +247,7 @@ const Diseases = ()=>{
                     </h1>
                 </div>
          
-                 <Col style={{background: '#ECDFD7', borderRadius: '20px', padding: '100px'}} span={24}>
+                 <Col style={{background: '#ECDFD7', borderRadius: '20px', padding: '50px'}} span={24}>
              
                  <Carousel
             additionalTransfrom={0}
@@ -303,7 +303,7 @@ const Diseases = ()=>{
             swipeable
           >
                      {testimonials.map((item) => 
-                              <div style={{background: '#F6F5EC',  borderRadius: '20px', margin: '20px', padding: '30px', width: '80%', margin: 'auto'}}>
+                              <div style={{background: '#F6F5EC',  borderRadius: '20px', margin: '20px', padding: '30px', width: '95%',}}>
                              <div style={{display: 'flex'}}> 
                                     <img  style={{height: '50px', width: '15%', borderRadius: '50%'}} src={`https://curevive.thotamali.com/${item.profilepic}`}></img>
                                     <div style={{padding: '0px 40px'}}>
@@ -477,7 +477,7 @@ const Diseases = ()=>{
             <Col span={16}>
                 <div style={{background: '#ECDFD7', borderRadius: '30px', marginTop: '100px', padding: '70px'}}>
                     <h1 style={{textAlign: 'center', fontFamily: "Playfair Display", fontSize: "50px"}}>Check out the questions asked by our patients previously</h1>
-                    <h3 style={{textAlign: 'center', fontFamily: "Playfair Display", fontSize: "25px"}}>Visit Q&A </h3>
+                    <h3 style={{textAlign: 'center', fontFamily: "Playfair Display", fontSize: "25px"}}>Visit Q & A </h3>
                 </div>
             </Col>
             <Col span={4}></Col>
